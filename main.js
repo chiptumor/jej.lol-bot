da3984b5abde44cf08ad3a36c00381f67cdced56
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-07-24 04:21:00 -0500

    feat: name anonymous functions

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ const client = new Discord.Client({ intents: [
     Discord.GatewayIntentBits.MessageContent
 ]});
 
-client.on(Discord.Events.InteractionCreate, function (interaction) {
+client.on(Discord.Events.InteractionCreate, function onCommand(interaction) {
     getPost(interaction.options.getString("query"))
         .then(response => interaction.reply(response));
 });
-client.on(Discord.Events.MessageCreate, function (message) {
+client.on(Discord.Events.MessageCreate, function onMessage(message) {
     if (!message.content.match(/^::/m)) return;
 
     getPost(message.content.match(/^::+(.*)/m)[1])
